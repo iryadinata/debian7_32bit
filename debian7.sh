f4694434c63e84037ace5a3fca7df96df6a2f55a
Iryadinata <iryadinatta@gmail.com>
2017-06-15 03:37:06 +0700

    Create debian7.sh

--- debian7.sh
+++ debian7.sh
@@ -79,10 +79,12 @@ wget -O /etc/openvpn/1194.conf "https://raw.githubusercontent.com/iryadinata/deb
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-iptables -t nat -I POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
-iptables-save > /etc/iptables_yg_baru_dibikin.conf
-wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/iryadinata/debian7_32bit/master/openvpn/iptables"
-chmod +x /etc/network/if-up.d/iptables
+wget -O /etc/iptables.up.rules "https://raw.githubusercontent.com/iryadinata/debian7_32bit/master/openvpn/iptables"
+sed -i '$ i\iptables-restore < /etc/iptables.up.rules' /etc/rc.local
+MYIP=`curl -s ifconfig.me`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+sed -i $MYIP2 /etc/iptables.up.rules;
+iptables-restore < /etc/iptables.up.rules
 service openvpn restart
 
 #konfigurasi openvpn
